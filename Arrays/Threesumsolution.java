75ae81fb0ff3fc633a4e9cc0e2c983c0a4b3bb14
bhuvan <bhuvanagra14@gmail.com>
2026-03-16 12:45:44 +0000

    radhe

--- Arrays/Threesumsolution.java
+++ Arrays/Threesumsolution.java
@@ -1,3 +1,6 @@
+//Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+// The solution set must not contain duplicate triplets.
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
