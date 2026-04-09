ea390dc311d13817aab331780a355b879548a9a3
bhuvan <bhuvanagra14@gmail.com>
2026-04-09 13:59:04 +0000

    radhey

---
+++ HashingAndHeaps/K_Largest_Elements.java
@@ -0,0 +1,39 @@
+package HashingAndHeaps;
+import java.util.*;
+
+public class K_Largest_Elements {
+
+    public static int[] findKLargest(int[] arr, int k) {
+        // STEP 1: Create Min Heap
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        // STEP 2: Process array
+        for (int num : arr) {
+
+            pq.add(num); // add element
+
+            // If size exceeds k → remove smallest
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        // STEP 3: Extract result
+        int[] result = new int[k];
+
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = pq.poll(); // fill in reverse for descending order
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+
+        int[] result = findKLargest(arr, k);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
